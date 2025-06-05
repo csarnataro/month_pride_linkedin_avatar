6f1214360697b5c33c7a701d09a26591200cc308
Christian <christian@mezzanine.local>
2025-06-06 00:13:45 +0200

    Added text

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const colors = [
   '#e50000',
-   '#ff8dd0',
-   '#ffee00',
-   '#028121',
-   '#004cff',
-   '#770088',
+  '#ff8dd0',
+  '#ffee00',
+  '#028121',
+  '#004cff',
+  '#770088',
 ];
 window.onload = function() {
   var canvas = document.getElementById('imageCanvas');
@@ -21,7 +21,6 @@ window.onload = function() {
         canvas.height = img.height;
         ctx.drawImage(img, 0, 0);
         setTimeout(() => { 
-          console.log('LOADED');
           drawFlag(colors);
         }, 100);
       };
@@ -29,24 +28,7 @@ window.onload = function() {
     };
     reader.readAsDataURL(e.target.files[0]);
   });
-  canvas.addEventListener('mousedown', function(e) {
-    mousePressed = true;
-    draw(e.offsetX, e.offsetY, false);
-  });
 
-  canvas.addEventListener('mousemove', function(e) {
-    if (mousePressed) {
-      draw(e.offsetX, e.offsetY, true);
-    }
-  });
-
-  canvas.addEventListener('mouseup', function() {
-    mousePressed = false;
-  });
-
-  canvas.addEventListener('mouseleave', function() {
-    mousePressed = false;
-  });
 
   document.getElementById('saveBtn').addEventListener('click', function() {
     var dataURL = canvas.toDataURL('image/png').replace("image/png", "image/octet-stream");
@@ -56,40 +38,25 @@ window.onload = function() {
     link.click();
   });
 
-  function draw(x, y, isDown) {
-    if (isDown) {
-      console.log(x, y);
-      ctx.beginPath();
-      ctx.strokeStyle = 'red'; // Change drawing color here
-      ctx.lineWidth = 3; // Change drawing line width here
-      ctx.lineJoin = 'round';
-      ctx.moveTo(lastX, lastY);
-      ctx.lineTo(x, y);
-      ctx.closePath();
-      ctx.stroke();
-    }
-    lastX = x; lastY = y;
-  }
-
   function drawArc(x, y, color, startAngle) {
     ctx.beginPath();
     let endAngle = startAngle + Math.PI / 6;
     let realStartAngle = startAngle;
-    if (color == colors[0]) {
-      const grad=ctx.createLinearGradient(x * 1.3, y + 130, x * 1.7, y + 90);
+    if (color === colors[0]) {
+      const grad=ctx.createLinearGradient(x * 1.5, y + 125, x * 1.6, y + 90);
 
       grad.addColorStop(0, color);
       grad.addColorStop(1, "transparent");
       ctx.strokeStyle = grad;
       realStartAngle = startAngle - Math.PI / 6;
       endAngle = realStartAngle + 2 * (Math.PI / 6);
-    } else if (color == colors[colors.length - 1]){
-      const grad=ctx.createLinearGradient(x * 1.3, y + 130, x * 1.7, y + 90);
+    } else if (color === colors[colors.length - 1]){
+      const grad=ctx.createLinearGradient(x * 0.5, y - 110, x * 0.25, y - 80);
 
-      grad.addColorStop(0, color);
-      grad.addColorStop(1, "transparent");
+      grad.addColorStop(1, color);
+      grad.addColorStop(0, "transparent");
       ctx.strokeStyle = grad;
-      realStartAngle = startAngle - Math.PI / 6;
+      realStartAngle = startAngle; // - Math.PI / 6;
       endAngle = realStartAngle + 2 * (Math.PI / 6);
     } else {
       ctx.strokeStyle = color;
@@ -102,15 +69,57 @@ window.onload = function() {
 
   function drawFlag(colors) {
     const startAngle = Math.PI / 4;
+    const centerX = img.width / 2;
+    const centerY = img.height / 2;
     for (let i=0; i < colors.length; i++) {
       drawArc(
-        img.width / 2, 
-        img.height / 2, 
+        centerX, 
+        centerY,
         colors[i], 
         startAngle + i * Math.PI / 6 
       );
     }
-    ctx.fillRect((img.width / 2 ) * 1.3, img.height / 2 + 130, 3, 3);
-    ctx.fillRect((img.width / 2 ) * 1.7, img.height / 2 + 90, 3, 3);
+    //    ctx.font = "bold 30px Serif";
+    //    ctx.fillStyle = 'white';
+    //    ctx.fillTextCircle("#pridemonth ", centerX, centerY, 75, Math.PI / 2);
+
+    // ctx.fillRect((img.width / 2 ) * 0.5, img.height / 2 - 110, 3, 3);
+    // ctx.fillRect((img.width / 2 ) * 0.25, img.height / 2 - 80, 3, 3);
+    drawTextAlongArc(ctx, "#PRIDEMONTH", centerX, centerY, centerX * 0.9, 2.1 * Math.PI, 26);
   }
+}
+
+
+function drawTextAlongArc(context, str, centerX, centerY, radius, angle, fontSizeInPoints) {
+  var len = str.length, s, letterAngle;
+  if (!fontSizeInPoints) {
+    fontSizeInPoints = 30; 
+  }
+  context.save();
+  context.fillStyle = 'white';
+  context.font = fontSizeInPoints + "pt Arial";
+  context.textAlign = 'center';
+  context.translate(centerX, centerY);
+  context.rotate(angle + Math.PI / 2);
+
+  for (var n = 0; n < len; n++) {
+    s = str[n];
+    letterAngle = -0.7*(context.measureText(s).width / radius);
+
+    context.rotate(letterAngle);
+    context.save();
+
+    context.translate(0, radius);
+    context.strokeStyle = '#333';
+    context.lineWidth = 1;
+    context.strokeText(s, 1, 1);
+    context.strokeStyle = '#ddd';
+    context.lineWidth = 1;
+    context.strokeText(s, -1, -1);
+    context.fillText(s, 0, 0);
+    context.restore();
+
+    context.rotate(letterAngle);
+  }
+  context.restore();
 }
